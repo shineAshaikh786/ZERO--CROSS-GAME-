42efb0ad3df96f415e08b88ebb17b571bd017261
shineAshaikh786 <144598137+shineAshaikh786@users.noreply.github.com>
2024-02-07 14:57:06 +0530

    Delete third.js

--- third.js
+++
@@ -1,8 +0,0 @@
-// PRACTICE QUE 1 AND 2 OF LECTURE 3 ( STRINGS )
-// QUE 1 : GENERATED A USER NAME WITH PROMPT FROM THEM BASED ON INPUT ATART USERNAME WITH @, 
-//FOLLOWED BY THEIR FULL NAME AND ENDING WITH THE FULL NAME LENGTH 
-//EG : @SHAIKHS7
-let fullName = prompt("enter your fullName without spaces :");
-let username = "@" + fullName + fullName.length;
-console.log(username);
-
